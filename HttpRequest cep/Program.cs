5deb48fad778a7ac9209712d37caf3569e3676ee
daniel <xdanielrodrigues@hotmail.com>
2022-03-23 11:49:02 -0300

    HttpRequest alimentando endereço com json retornado. Classes de acesso a web e stream

---
+++ HttpRequest cep/Program.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System.Xml;
+
+await ExecutarAsync();
+
+async Task ExecutarAsync()
+{
+    Console.WriteLine("digite seu cep\n");
+    var cep = Console.ReadLine();
+    Console.WriteLine("\n -----------JSON----------- \n");
+
+    using (var client = new HttpClient())
+    {
+        var json = await client.GetStringAsync(@$"https://viacep.com.br/ws/{cep}/json/");
+        var end = JsonConvert.DeserializeObject<Endereco>(json);
+
+        Console.WriteLine(@$"{end.Cep} | {end.Logradouro} | {end.Complemento} | {end.Localidade} | {end.Bairro} | {end.UF} | {end.Ibge} | {end.Gia}");
+    }
+
+    Console.WriteLine("\n -----------XML----------- \n");
+
+    using (var client = new HttpClient())
+    {
+        var xml = await client.GetStringAsync($"https://viacep.com.br/ws/{cep}/xml/");
+        using (var leitor = new StringReader(xml))
+        {
+            var leitorxml = new XmlTextReader(leitor);
+            while(leitorxml.Read())
+            {
+                Console.WriteLine($"tipo:{leitorxml.NodeType} | {leitorxml.Name}");
+            }
+        }
+  
+    }
+
+}
+
+
+public class Endereco
+{
+    public string Cep { get; set; }
+    public string Logradouro { get; set; }
+    public string Complemento { get; set; }
+    public string Bairro { get; set; }
+    public string Localidade { get; set; }
+    public string UF { get; set; }
+    public string Ibge { get; set; }
+    public string Gia { get; set; }
+    public int DDD { get; set; }
+    public string Siafi { get; set; }
+
+}
---
+++ web/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+
+
+
+//FirstAsync();
+//Second();
+await ThirdAsync();
+
+// retorna o html do site usando WebClient de forma assincrona
+static async Task FirstAsync()
+{
+    WebClient client = new WebClient();
+    var stringdownloaded = await client.DownloadStringTaskAsync("https://balta.io");
+    Console.WriteLine(stringdownloaded);
+}
+
+// retorna o html do site usando WebRequest
+static void Second()
+{
+    var request = WebRequest.Create("https://balta.io");
+    var response = request.GetResponse();
+    using (var responseReader = new StreamReader(response.GetResponseStream()))
+    {
+        var siteText = responseReader.ReadToEnd();
+        Console.WriteLine(siteText);
+    }
+}
+
+// retorna o html do site usando HttRequest
+static async Task ThirdAsync()
+{
+    var client = new HttpClient();
+    var stringdownloaded = await client.GetStringAsync("https://balta.io");
+    Console.WriteLine(stringdownloaded);
+}
